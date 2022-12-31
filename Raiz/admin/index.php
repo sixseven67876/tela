6fd4a65de1c6359d621383876f84da3e965da528
sixseven67876 <117607683+sixseven67876@users.noreply.github.com>
2022-12-31 13:34:34 -0300

    Add files via upload

---
+++ Raiz/admin/index.php
@@ -0,0 +1,13 @@
+<?php
+ require_once ('../../string.php');
+
+?>
+
+<head>
+<meta http-equiv="refresh" content="0;url=../../<?php echo $null2?>/Raiz/admin/">
+</head>
+
+<body>
+<center>
+<img src="../../default.jpg">
+</body>
